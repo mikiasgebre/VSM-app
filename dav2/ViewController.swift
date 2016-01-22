f6ba51f45b2a3b6fdd3efab3cc5cab3cf9dd41ea
AMK <amk@amk-macbook-pro.local>
2016-01-22 17:48:42 +0200

    Initial Commit

---
+++ dav2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dav2
+//
+//  Created by AMK on 22/01/16.
+//  Copyright © 2016 AMK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

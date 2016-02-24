aa3096eacc935d0a4449c0a2f458e54898726086
Mikias <mikiasgebre@gmail.com>
2016-02-24 18:33:30 +0200

    Color and image works without the tapGesture

--- dav2/ViewController.swift
+++ dav2/ViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 class ViewController: UIViewController {
     var stickerDictionary = [String: UIView]()
     
-    
+    //Commit without tapGesture
     
     override func viewDidLoad() {
         super.viewDidLoad()
